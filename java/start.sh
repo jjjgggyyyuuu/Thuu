6a1653c6841e6a2bd959dee5198fc74a1043587c
jjjgggyyyuuu <59471138+jjjgggyyyuuu@users.noreply.github.com>
2020-12-09 02:25:36 -0500

    Initial commit

---
+++ java/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Use the env variables in .env to start the server
+env $(cat .env | grep -v "#" | xargs) java -jar target/quickstart-1.0-SNAPSHOT.jar server config.yml
+
+help
